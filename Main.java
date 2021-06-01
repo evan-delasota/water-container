e327c09a606c1623f7516d28910fc653eb83e173
Evan D <Evan_delasota@yahoo.com>
2021-06-01 04:53:02 +0000

    fixed for loop typo

--- Main.java
+++ Main.java
@@ -11,9 +11,9 @@ class Main {
 
     int max = Integer.MIN_VALUE;
 
-    for (int i = 0; i < arr.height; ++i) {
+    for (int i = 0; i < arr.length; ++i) {
       int curr = arr[i];
-      for (int j = i + 1; j < arr.height; ++j) {
+      for (int j = i + 1; j < arr.length; ++j) {
         int height = j - i;
         int width = Math.min(arr[j], curr);
         if (height * width > max) {
