49a135d34b13b8c4b355b049d01a8ec7c4b3155a
Evan D <Evan_delasota@yahoo.com>
2021-06-01 05:06:10 +0000

    Two-pointer optimized solution, O(n) runtime

--- Main.java
+++ Main.java
@@ -10,15 +10,17 @@ class Main {
     }
 
     int max = Integer.MIN_VALUE;
+    int front = 0;
+    int tail = arr.length - 1; 
 
-    for (int i = 0; i < arr.length; ++i) {
-      int curr = arr[i];
-      for (int j = i + 1; j < arr.length; ++j) {
-        int height = j - i;
-        int width = Math.min(arr[j], curr);
-        if (height * width > max) {
-          max = height * width;
-        }
+    while (front < tail) {
+      int height = tail - front;
+      int width = Math.min(arr[tail], arr[front]);
+      max = Math.max(max, width * height);
+      if (arr[front] > arr[tail]) {
+        tail--;
+      } else {
+        front++;
       }
     }
 
