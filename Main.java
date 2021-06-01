c816de934f6043c11a4202403f297647dfbd9057
Evan D <Evan_delasota@yahoo.com>
2021-06-01 04:50:03 +0000

    Brute force solution, O(n^2)

--- Main.java
+++ Main.java
@@ -1,5 +1,29 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    int[] a = {7, 1, 2, 3 , 9};
+    System.out.println(mostWater(a));
+  }
+
+  public static int mostWater(int[] arr) {
+    if (arr.length < 2) {
+      return 0;
+    }
+
+    int max = Integer.MIN_VALUE;
+
+    for (int i = 0; i < arr.length; ++i) {
+      int curr = arr[i];
+      for (int j = i + 1; j < arr.length; ++j) {
+        int length = j - i;
+        int width = Math.min(arr[j], curr);
+        if (length * width > max) {
+          max = length * width;
+          System.out.println("width: " + width);
+          System.out.println("length: " + length);
+        }
+      }
+    }
+
+    return max;
   }
 }
